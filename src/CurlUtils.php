8bcf8c624f278e6eabd3b5a73db30ff39f2f66e9
Joe <joe@xxtime.com>
2017-09-08 16:07:33 +0800

    agent

--- src/CurlUtils.php
+++ src/CurlUtils.php
@@ -25,7 +25,7 @@ class CurlUtils
         CURLOPT_COOKIEFILE     => null,
         CURLOPT_COOKIEJAR      => null,
         CURLOPT_HTTPHEADER     => ['accept-language: en-US,en;q=0.8', 'Cookie: locale=en_US'],
-        CURLOPT_USERAGENT      => 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+        CURLOPT_USERAGENT      => 'CurlUtils (XT) https://github.com/xxtime/curl-utils',
     ];
 
     // curl output info
